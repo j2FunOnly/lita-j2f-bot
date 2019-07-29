b8d0909076bcff650ebf6900dff21eace3ebc2f7
j2FunOnly <j2funonly@ya.ru>
2019-07-29 13:49:19 +0300

    Hello slack adapter

--- Gemfile
+++ Gemfile
@@ -4,4 +4,8 @@ ruby '2.5.5'
 gem "lita"
 # rvm-gemset=lita
 
+group :production do
+  gem 'lita-slack'
+end
+
 gem 'lita-j2f-capslocker', git: 'https://github.com/j2FunOnly/lita-j2f-capslocker.git'
--- lita_config.rb
+++ lita_config.rb
@@ -3,7 +3,7 @@ Lita.configure do |config|
   config.robot.name = "Lita"
 
   # The locale code for the language to use.
-  config.robot.locale = :en
+  # config.robot.locale = :en
 
   # The severity of messages to log. Options are:
   # :debug, :info, :warn, :error, :fatal
@@ -17,17 +17,11 @@ Lita.configure do |config|
 
   # The adapter you want to connect with. Make sure you've added the
   # appropriate gem to the Gemfile.
-  config.robot.adapter = :shell
-
-  ## Example: Set options for the chosen adapter.
-  # config.adapter.username = "myname"
-  # config.adapter.password = "secret"
-
-  ## Example: Set options for the Redis connection.
-  # config.redis.host = "127.0.0.1"
-  # config.redis.port = 1234
-
-  ## Example: Set configuration for any loaded handlers. See the handler's
-  ## documentation for options.
-  # config.handlers.some_handler.some_config_key = "value"
+  if ENV['RACK_ENV'] == 'production'
+    config.robot.adapter = :slack
+    config.adapters.slack.token = ENV.fetch('SLACK_TOKEN', '')
+    config.redis[:url] = ENV.fetch('REDIS_URL')
+  else
+    config.robot.adapter = :shell
+  end
 end
